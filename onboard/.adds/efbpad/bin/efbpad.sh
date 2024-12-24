6baf03a29faa5fc4b832e3f70172ceb2c2235d4e
Christian Chapman <user@debian>
2024-12-24 16:30:01 -0500

    corrected kobo scripts

--- onboard/.adds/efbpad/bin/efbpad.sh
+++ onboard/.adds/efbpad/bin/efbpad.sh
@@ -1,19 +1,38 @@
-#!/bin/sh
+#!/bin/sh 
 
 export EFBPAD_INSTALL_PREFIX="/mnt/onboard/.adds/efbpad"
 export PATH="$EFBPAD_INSTALL_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$EFBPAD_INSTALL_PREFIX/lib:$LD_LIBRARY_PATH"
 export KB_INPUT="/dev/input/event3" # This shouldn't be hardcoded
 
-echo 2 > "/sys/class/graphics/fb0/rotate" # Landscape
+# Set up screen
+export OLD_FB_DEPTH="$(fbdepth -g)"
+export OLD_FB_ROTA="$(fbdepth -o)"
+fbdepth -d 32 -r 2 # on clara bw: 0,2 = landscape, 3=portrait, 1=upside down
+
+# Ensure bluetooth is up
+if ! pgrep "mtkbtd" > /dev/null; then
+    echo "Starting mtkbtd..."
+    /usr/local/Kobo/mtkbtd-launcher.sh &
+fi
+dbus-send --system --print-reply --dest=com.kobo.mtk.bluedroid /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:"org.bluez.Adapter1" string:"Powered"  variant:boolean:true
+
+# Wait for keyboard to connect
+if [ ! -c $KB_INPUT ]; then
+    sleep 3 
+fi
+
+# If we got a keyboard, start koreader
 if [ -c $KB_INPUT ]; then
     kbreader $KB_INPUT | fbpad tmux new-session -A -s main
+    dbus-send --system --print-reply --dest=com.kobo.mtk.bluedroid /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:"org.bluez.Adapter1" string:"Powered"  variant:boolean:false
     RETURN_VALUE=$?
 else
     echo "Device $KB_INPUT not found. Doing cleanup and exit."
     RETURN_VALUE=1
 fi
-echo 3 > "/sys/class/graphics/fb0/rotate" # Portrait
 
-exit ${RETURN_VALUE}
+# Revert screen
+fbdepth -d "$OLD_FB_DEPTH" -r "$OLD_FB_ROTA"
 
+exit $RETURN_VALUE
