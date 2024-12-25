17c0a64b4a920c9c800a5873f69b724acbfa9b0f
Christian Chapman <user@debian>
2024-12-25 09:49:39 -0500

    corrected efbpad startup script

--- onboard/.adds/efbpad/bin/efbpad.sh
+++ onboard/.adds/efbpad/bin/efbpad.sh
@@ -5,14 +5,8 @@ export PATH="$EFBPAD_INSTALL_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$EFBPAD_INSTALL_PREFIX/lib:$LD_LIBRARY_PATH"
 export KB_INPUT="/dev/input/event3" # This shouldn't be hardcoded
 
-# Ensure bluetooth is up
-if ! pgrep "mtkbtd" &> /dev/null; then
-    echo "efbpad: Starting mtkbtd..."
-    /usr/local/Kobo/mtkbtd-launcher.sh &
-fi
+echo "[efbpad] Pulling up bluetooth, finding keyboard"
 dbus-send --system --print-reply --dest=com.kobo.mtk.bluedroid /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:"org.bluez.Adapter1" string:"Powered"  variant:boolean:true
-
-# Wait 5 seconds for the keyboard to connect
 for idx in $(seq 1 50); do
     if [ -c $KB_INPUT ]; then
 	break
@@ -21,24 +15,23 @@ for idx in $(seq 1 50); do
 done
 
 # If we got a keyboard, start koreader
-if [ -c $KB_INPUT ]; then
-    
-    # Set up screen
-    export OLD_FB_DEPTH="$(fbdepth -g)"
-    export OLD_FB_ROTA="$(fbdepth -o)"
-    fbdepth -d 32 -r 2 # on clara bw: 0,2 = landscape, 3=portrait, 1=upside down
-
-    #kbreader $KB_INPUT | fbpad tmux new-session -A -s main
-    kbreader $KB_INPUT | fbpad /bin/sh
-    dbus-send --system --print-reply --dest=com.kobo.mtk.bluedroid /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:"org.bluez.Adapter1" string:"Powered"  variant:boolean:false
-    RETURN_VALUE=$?
-
-    # Revert screen
-    fbdepth -d "$OLD_FB_DEPTH" -r "$OLD_FB_ROTA"
-    
-else
-    >&2 echo "efbpad: Device $KB_INPUT not found. Doing cleanup and exit."
+if [ ! -c $KB_INPUT ]; then
+    >&2 echo "[efbpad] Device $KB_INPUT not found. Doing cleanup and exit."
     RETURN_VALUE=1
 fi
 
-exit $RETURN_VALUE
+echo "[efbpad] Configuring framebuffer"
+export OLD_FB_DEPTH="$(fbdepth -g)"
+export OLD_FB_ROTA="$(fbdepth -o)"
+fbdepth -d 32 -r 2 # on clara bw: 0,2 = landscape, 3=portrait, 1=upside down
+
+echo "[efbpad] Starting kbreader, fbpad"
+# This command will continue until the pipe breaks
+#kbreader $KB_INPUT | fbpad tmux new-session -A -s main
+kbreader $KB_INPUT | fbpad /bin/sh
+
+echo "[efbpad] Cleaning up: turning off bluetooth and resetting framebuffer"
+dbus-send --system --print-reply --dest=com.kobo.mtk.bluedroid /org/bluez/hci0 org.freedesktop.DBus.Properties.Set string:"org.bluez.Adapter1" string:"Powered"  variant:boolean:false
+fbdepth -d "$OLD_FB_DEPTH" -r "$OLD_FB_ROTA"
+    
+exit 0
