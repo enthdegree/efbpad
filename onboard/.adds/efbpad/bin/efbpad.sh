026067d0ab531ed8c282d711907cd16849befbb1
Christian Chapman <user@debian>
2025-01-10 00:54:27 -0500

    updated to latest version + patch of fbpad, fixed bad LD_LIBRARY_PATH

--- onboard/.adds/efbpad/bin/efbpad.sh
+++ onboard/.adds/efbpad/bin/efbpad.sh
@@ -5,7 +5,11 @@ export EFBPAD_CMD="/bin/sh"
 export EFBPAD_INSTALL_PREFIX="/mnt/onboard/.adds/efbpad"
 export KB_INPUT="/dev/input/event3" # This shouldn't be hardcoded
 export PATH="$EFBPAD_INSTALL_PREFIX/bin:$PATH"
-export LD_LIBRARY_PATH="$EFBPAD_INSTALL_PREFIX/lib:$LD_LIBRARY_PATH"
+if [ -z "$LD_LIBRARY_PATH" ]; then
+    export LD_LIBRARY_PATH="$EFBPAD_INSTALL_PREFIX/lib"
+else
+    export LD_LIBRARY_PATH="$EFBPAD_INSTALL_PREFIX/lib:$LD_LIBRARY_PATH"
+fi
 TF_LOCATION="/mnt/onboard/fonts/tf"
 EFBPAD_FONT=""
 
